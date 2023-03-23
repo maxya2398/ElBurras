48aeb7fa61d45347be4d13e5ebbeb6d62bb4cc94
Maximiliano <max-y-a@hotmail.com>
2023-03-22 21:42:54 -0600

    Prueba de diseño #1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    display_ct();
+    platillos();
+})
 function display_c() {
     var refresh = 1000; // Refresh rate in milli seconds
     mytime = setTimeout('display_ct()', refresh)
@@ -20,3 +24,71 @@ function display_ct() {
     }
     display_c();
 }
+
+const imagenes = document.querySelectorAll('img');
+const btnTodos = document.querySelector('.todos');
+const btnBurritos = document.querySelector('.burritosBtn');
+const btnGringas = document.querySelector('.gringasBtn');
+const btnTacos = document.querySelector('.tacosBtn');
+const btnTortas = document.querySelector('.tortasBtn');
+const contenedorPlatillos = document.querySelector('.platillos');
+
+
+imagenes.forEach(imagen => {
+    imagen.src = imagen.dataset.src;
+})
+const platillos = () => {
+    let platillosArreglo = [];
+    const platillos = document.querySelectorAll('.platillo');
+    platillos.forEach(platillo => {
+        platillosArreglo = [...platillosArreglo, platillo]
+    })
+    const burritos = platillosArreglo.filter(burrito => burrito.getAttribute('data-platillo') === 'burrito');
+    const gringas = platillosArreglo.filter(gringa => gringa.getAttribute('data-platillo') === 'gringa');
+    const tacos = platillosArreglo.filter(tacos => tacos.getAttribute('data-platillo') === 'tacos');
+    const tortas = platillosArreglo.filter(tortas => tortas.getAttribute('data-platillo') === 'tortas');
+    mostrarPlatillos(burritos, gringas, tacos, tortas, platillosArreglo)
+}
+const mostrarPlatillos = (burritos, gringas, tacos, tortas, todos) => {
+    btnBurritos.addEventListener('click', () => {
+        limpiarHtml(contenedorPlatillos)
+        burritos.forEach(burrito => contenedorPlatillos.appendChild(burrito))
+    })
+
+    btnGringas.addEventListener('click', () => {
+        limpiarHtml(contenedorPlatillos)
+        gringas.forEach(gringa => contenedorPlatillos.appendChild(gringa))
+    })
+
+    btnTacos.addEventListener('click', () => {
+        limpiarHtml(contenedorPlatillos)
+        tacos.forEach(taco => contenedorPlatillos.appendChild(taco))
+    })
+
+    btnTortas.addEventListener('click', () => {
+        limpiarHtml(contenedorPlatillos)
+        tortas.forEach(torta => contenedorPlatillos.appendChild(torta))
+    });
+
+    btnTodos.addEventListener('click', () => {
+        limpiarHtml(contenedorPlatillos)
+        todos.forEach(todo => contenedorPlatillos.appendChild(todo))
+    })
+}
+const limpiarHtml = (contenedor) => {
+    while (contenedor.firstChild) {
+        contenedor.removeChild(contenedor.firstChild);
+
+    }
+}
+//seccion videos, checar si funciona mejor luego
+// let videoBtn = document.querySelectorAll('.vid-btn');
+// videoBtn.forEach((btn) => {
+//     console.log('hey')
+//     btn.addEventListener('click', () => {
+//         document.querySelector('.controls .active').classList.remove('active');
+//         btn.classList.add('active');
+//         let src = btn.getAttribute('data-src');
+//         document.querySelector('#video-slider').src = src;
+//     });
+// });
